41521d02c3f0d8f1c448672f11216b20b2d35084
marykrisv <marykrisvill@gmail.com>
2022-08-18 01:41:14 +0800

    remove unused imports

--- src/main/java/com/formedix/forex/map/ForexMap.java
+++ src/main/java/com/formedix/forex/map/ForexMap.java
@@ -1,7 +1,6 @@
 package com.formedix.forex.map;
 
 import com.formedix.forex.utils.CsvReader;
-import lombok.extern.slf4j.Slf4j;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
